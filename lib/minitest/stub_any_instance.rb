8b78f8fa0e2204ca3d871fca6b70e59b36ab6561
Mike Vastola <Mike.Vastola@dailypay.com>
2021-10-06 12:09:03 -0400

    Add support for keyword arguments
    Drops support for rubies < 2.0

--- lib/minitest/stub_any_instance.rb
+++ lib/minitest/stub_any_instance.rb
@@ -1,14 +1,14 @@
 class BasicObject
-  def self.stub_any_instance name, val_or_callable = nil, &block
+  def self.stub_any_instance(name, val_or_callable = nil, &block)
     new_name = "__minitest_any_instance_stub__#{name}"
 
     owns_method = instance_method(name).owner == self
     class_eval do
       alias_method new_name, name if owns_method
 
-      define_method(name) do |*args|
-        if val_or_callable.respond_to? :call then
-          instance_exec(*args, &val_or_callable)
+      define_method(name) do |*args, **kwargs|
+        if val_or_callable.respond_to?(:call)
+          instance_exec(*args, **kwargs, &val_or_callable)
         else
           val_or_callable
         end
--- minitest-stub_any_instance.gemspec
+++ minitest-stub_any_instance.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.test_files    = ["test/stub_any_instance_test.rb"]
   spec.require_paths = ["lib"]
 
+  spec.required_ruby_version    = '>= 2.0'
   spec.add_development_dependency "bundler", ">= 1.11.2"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "minitest"
