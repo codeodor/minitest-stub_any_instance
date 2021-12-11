041478b756adb4efb6700b2a606271f1129b4bed
Sammy Larbi <sam@codeodor.com>
2021-12-10 19:06:19 -0600

    Fix for Ruby < 3.0 after #17 merged

--- lib/minitest/stub_any_instance.rb
+++ lib/minitest/stub_any_instance.rb
@@ -6,11 +6,21 @@ class BasicObject
     class_eval do
       alias_method new_name, name if owns_method
 
-      define_method(name) do |*args, **kwargs|
-        if val_or_callable.respond_to?(:call)
-          instance_exec(*args, **kwargs, &val_or_callable)
-        else
-          val_or_callable
+      if ::RUBY_VERSION >= "3"
+        define_method(name) do |*args, **kwargs|
+          if val_or_callable.respond_to?(:call)
+            instance_exec(*args, **kwargs, &val_or_callable)
+          else
+            val_or_callable
+          end
+        end
+      else
+        define_method(name) do |*args|
+          if val_or_callable.respond_to?(:call)
+            instance_exec(*args, &val_or_callable)
+          else
+            val_or_callable
+          end
         end
       end
     end
--- lib/minitest/stub_any_instance/version.rb
+++ lib/minitest/stub_any_instance/version.rb
@@ -1,5 +1,5 @@
 module Minitest
   module StubAnyInstance
-    VERSION = "1.0.2"
+    VERSION = "1.0.3"
   end
 end
